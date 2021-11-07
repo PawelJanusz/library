eba6df2bd21bfb218d56ef47bc5f720568a88f25
Pawel Janusz <marek80k@wp.pl>
2021-11-07 15:05:01 +0100

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         mainMenu();
 
     }
-    static List<Book> book1List = new LinkedList<>();
-    static List<Book> book3List = Arrays.asList(new Book("quo vadis", "Henryk Sienkiewicz", 400 ),
+    static List<Book> emptyList = new LinkedList<>();
+    static List<Book> booksList = Arrays.asList(new Book("quo vadis", "Henryk Sienkiewicz", 400 ),
             new Book("clean code", "Robert Martin", 250),
             new Book("headway", "Liz Soars", 80));
 
     static Library library = new Library();
-    static Reader reader = new Reader("Adam", "Nowak", 23, 654789346, book1List);
+    static Reader reader = new Reader("Adam", "Nowak", 23, 654789346, emptyList);
 
 
 
@@ -30,7 +30,7 @@ public class Main {
                     "6.Borrow book \n" +
                     "7.Print borrowed books \n" +
                     "8.Return book \n" +
-                    "10.Quit");
+                    "9.Quit");
             command = scanner.nextLine();
             if (command.equals("1")) {
                 library.createUser(reader);
@@ -48,15 +48,15 @@ public class Main {
                 reader.removeUser(library);
             }
             if (command.equals("6")){
-                reader.borrowBook(book3List.get(2), library);
+                reader.borrowBook(booksList.get(2), library);
             }
             if (command.equals("7")){
                 reader.readBorrowedBooks();
             }
             if (command.equals("8")){
-                reader.returnBook(book3List.get(2),library);
+                reader.returnBook(booksList.get(2),library);
             }
-        } while (!command.equals("10"));
+        } while (!command.equals("9"));
     }
 }
 
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,5 +1,3 @@
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 
 import java.util.List;
